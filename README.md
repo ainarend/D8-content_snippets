a06e725dbc564c0b48058be3a7aece6a729dd335
Ain Arend <ainarend@gmail.com>
2018-01-19 16:47:41 +0200

    Permission & clean up

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # Content Snippets
+
+Allows usage of shortcodes
+
+## Usage
+```php
+/**
+ * Implements hook_snippets_info().
+ */
+function mymodule_snippets_info() {
+  $snippets = [];
+  $snippets['recent_nodes'] = [
+    'title' => t('Recent Nodes'),
+    'description' => t('List of recently updated nodes'),
+    'callback' => 'mymodule_recent_nodes_snippet'
+  ];
+  return $snippets;
+}
+
+/**
+ * The callback method for the recent_nodes snippet.
+ *
+ * @param $args
+ * @return
+ */
+function mymodule_recent_nodes_snippet($args) {
+  return [
+    '#type' => 'markup',
+    '#markup' => '<ul><li>My first node</li></ul>'
+  ];
+}
+```
--- src/Plugin/Filter/ContentSnippetFilter.php
+++ src/Plugin/Filter/ContentSnippetFilter.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * @file
+ * Contains Drupal\content_snippets\Plugin\Filter\ContentSnippetFilter.
+ */
 
 namespace Drupal\content_snippets\Plugin\Filter;
 
@@ -9,13 +13,23 @@ use Drupal\filter\Plugin\FilterBase;
 /**
  * @Filter(
  *   id = "filter_snippets",
- *   title = @Translation("Snippet Filter"),
+ *   title = @Translation("Content Snippet Filter"),
  *   description = @Translation("Replace shortcode with awesome stuff"),
  *   type = Drupal\filter\Plugin\FilterInterface::TYPE_MARKUP_LANGUAGE,
  * )
  */
-class ContentSnippetFilter extends FilterBase{
+class ContentSnippetFilter extends FilterBase {
 
+  /**
+   * Filtering for snippets.
+   *
+   * This method looks through the text to find snippets (with possible arguements)
+   * and replaces the snippet witt the response provided by snippet's callback method.
+   *
+   * @param string $text
+   * @param string $langcode
+   * @return FilterProcessResult
+   */
   public function process($text, $langcode) {
 
     $snippets = $this->getAllSnippets();
@@ -23,32 +37,34 @@ class ContentSnippetFilter extends FilterBase{
     $pattern = '/\[(' . implode("|", $snippets) . ')?.*(?:(?!\[]).)]/imU';
     $depth = 0;
 
-    while($depth < $this->settings['snippet_depth'] && preg_match_all($pattern, $text, $match)) {
+    while($depth < $this->settings['snippet_depth'] && preg_match_all($pattern, $text, $matched_snippet)) {
 
-      foreach ($match[0] as $snippet) {
+      if (!empty($matched_snippet[0])) {
+        // The matched_snippet contains an array where 0 is sthe full snippet with [ and ] included.
+        foreach ($matched_snippet[0] as $snippet) {
 
-        if (!isset($match[0])) {
-          continue;
-        }
+          // Check if the snippet contains arguements.
+          $args = [];
+          if (preg_match("/^\[(" . implode("|", array_keys($snippets)) . ") ?(.*)\]$/i", $snippet, $matched_snippet)) {
 
-        // Check if the snippet contains arguements.
-        $args = [];
-        if (preg_match("/^\[(" . implode("|", array_keys($snippets)) . ") ?(.*)\]$/i", $snippet, $match)) {
+            if (!empty($matched_snippet[2])) {
 
-          if (!empty($match[2])) {
-            $args = $this->getSnippetArguements($snippet, $match);
+              $args = $this->getSnippetArguements($matched_snippet);
+
+            }
+
           }
-        }
 
-        $snippet_info = $snippets[$match[1]];
+          $snippet_info = $snippets[$matched_snippet[1]];
 
-        if (array_key_exists('callback', $snippet_info) && function_exists($snippet_info['callback'])) {
-          $replace_data = call_user_func($snippet_info['callback'], $args);
-        }
+          if (array_key_exists('callback', $snippet_info) && function_exists($snippet_info['callback'])) {
+            $replace_data = call_user_func($snippet_info['callback'], $args);
+          }
 
-        if (isset($replace_data)) {
-          $renderer = \Drupal::service('renderer');
-          $text = str_replace($snippet, $renderer->render($replace_data), $text);
+          if (isset($replace_data)) {
+            $renderer = \Drupal::service('renderer');
+            $text = str_replace($snippet, $renderer->render($replace_data), $text);
+          }
         }
       }
 
@@ -59,6 +75,13 @@ class ContentSnippetFilter extends FilterBase{
     return new FilterProcessResult($text);
   }
 
+  /**
+   * Allows defining the maximum allowed recursion of snippets.
+   *
+   * @param array $form
+   * @param FormStateInterface $form_state
+   * @return array
+   */
   public function settingsForm(array $form, FormStateInterface $form_state) {
 
     $form['snippet_depth'] = [
@@ -72,6 +95,11 @@ class ContentSnippetFilter extends FilterBase{
 
   }
 
+  /**
+   * Invokes the snippets info hook for all modules to register their snippets.
+   *
+   * @return array
+   */
   public function getAllSnippets() {
 
     $snippets = \Drupal::moduleHandler()->invokeAll('snippets_info');
@@ -82,22 +110,30 @@ class ContentSnippetFilter extends FilterBase{
 
   }
 
-  public function getSnippetArguements($snippet, $match) {
+  /**
+   * This looks thorugh the s
+   *
+   * @param $match
+   * @return mixed
+   */
+  public function getSnippetArguements($snippet) {
 
     $pattern = '/(\\w+)\s*(\[])?=\\s*("[^"]*"|\'[^\']*\'|[^"\'\\s>]*)/';
 
-    preg_match_all($pattern, $match[2], $matches, PREG_SET_ORDER);
+    preg_match_all($pattern, $snippet, $matches, PREG_SET_ORDER);
 
-    foreach ($matches as $attr) {
+    if (!empty($matches)) {
+      foreach ($matches as $attr) {
 
-      if ($attr[2] == '[]') {
-        $args[$attr[1]] = explode(",", trim($attr[3], "\""));
+        if ($attr[2] == '[]') {
+          $args[$attr[1]] = explode(",", trim($attr[3], "\""));
+        }
+
+        else {
+          $args[$attr[1]] = trim($attr[3], "\"");
+        }
+
       }
-
-      else {
-        $args[$attr[1]] = trim($attr[3], "\"");
-      }
-
     }
 
     return $args;
